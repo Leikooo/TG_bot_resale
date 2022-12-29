2d969d094651706c6ea7d39382e0070ed37adb5a
SMSPOPER123 <ylanellsmm@gmail.com>
2022-12-29 23:51:09 +0300

    fix

--- keyboards/inline/user_func.py
+++ keyboards/inline/user_func.py
@@ -12,14 +12,14 @@ def open_item_func(position_id, remover, category_id, user_id):
     return open_item
 
 def check_payment_crystal(user_id):
-    check_payment_crystal_button1 = InlineKeyboardButton('Paid', callback_data=f"paid:{user_id}")
+    check_payment_crystal_button1 = InlineKeyboardButton('✅ Paid', callback_data=f"paid:{user_id}")
     check_payment_crystal_button2 = InlineKeyboardButton('Cancel payment', callback_data=f"otmena:{user_id}")
     return InlineKeyboardMarkup().add(check_payment_crystal_button1).add(check_payment_crystal_button2)
 
 # Подтверждение покупки товара
 def confirm_buy_items(amount_pay, user_id):
     confirm_buy_item_keyboard = InlineKeyboardMarkup()
-    yes_buy_kb = InlineKeyboardButton(text="✅ Confirm ✅",
+    yes_buy_kb = InlineKeyboardButton(text="✅ Confirm",
                                     callback_data=f"xbuy_item:{amount_pay}")
     not_buy_kb = InlineKeyboardButton("❌ Cancel ❌",
                                     callback_data=f"not_buy_items")
