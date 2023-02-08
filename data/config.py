a39ae1b544de13492262bbe4b11941e0856ec9a1
Никита <93408249+Leikooo@users.noreply.github.com>
2023-02-08 18:52:53 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,20 +1,19 @@
 # - *- coding: utf- 8 - *-
 
-token='5604019324:AAHRUoCprUM2yHE4rdTpum5pdC6guCD7JUw'
-BOT_TOKEN = token
+BOT_TOKEN=''
 
-admins = ['495338510']
+admins = ['']
 
-wallet_btc = 'bc1qannzganwjqtukqzp0j7a0np8yt58lcymhdrjct'
-wallet_ltc = 'LKk687iAXJw8tRJLBCeu2rSH8F5CwC4wPJ'
-wallet_eth = '0x0Eee47Fb672ca54efAee2149Aa670Eb291a78274'
+wallet_btc = ''
+wallet_ltc = ''
+wallet_eth = ''
 
-wallet_BNB1 = '0x0Eee47Fb672ca54efAee2149Aa670Eb291a78274' #Smart Chain (BEP20)
-wallet_BNB2 = 'bnb1ewt20ymxzey286uzm5lx6w5679suvmytx5hq5a' #Beacon Chain (BEP2) не ворк
+wallet_BNB1 = '' #Smart Chain (BEP20)
+wallet_BNB2 = '' #Beacon Chain (BEP2) не ворк
 
-wallet_usdt1 = '0x0Eee47Fb672ca54efAee2149Aa670Eb291a78274' #usdt ERC20 не ворк
-wallet_usdt2 = '0x0Eee47Fb672ca54efAee2149Aa670Eb291a78274' #usdt BEP20
-wallet_usdt3 = 'TGLKCzDstCz2h8pCyAZsiP7KRKZHjR2iZa' #Tron TRC20
+wallet_usdt1 = '' #usdt ERC20 не ворк
+wallet_usdt2 = '' #usdt BEP20
+wallet_usdt3 = '' #Tron TRC20
 
 min_cash=5
 
